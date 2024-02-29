711eaf7a812019df06825aff24c8dd116f8ac8ec
Jeremy GULLIET <jrmy.gulliet@gmail.com>
2024-02-29 17:51:23 +0100

    commit

--- App.js
+++ App.js
@@ -1,12 +1,11 @@
 import { StyleSheet, Text, View } from 'react-native';
-
-import { persistStore, persistReducer } from 'redux-persist';
-import { PersistGate } from 'redux-persist/integration/react';
 import storage from 'redux-persist/lib/storage';
-import AsyncStorage from '@react-native-async-storage/async-storage';
 
+import { Provider } from 'react-redux';
 import { combineReducers, configureStore } from '@reduxjs/toolkit';
-
+//import { persistStore, persistReducer } from 'redux-persist'; //persist Store React native à activer à la fin
+//import { PersistGate } from 'redux-persist/integration/react'; //persist Store React native à activer à la fin
+//import AsyncStorage from '@react-native-async-storage/async-storage'; //persist Store React native à activer à la fin
 
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
@@ -25,20 +24,23 @@ import UserScreen from './screens/UserScreen';
 import ValidateCameraScreen from './screens/ValidateCameraScreen';
 import ValidateImportScreen from './screens/ValidateImportScreen';
 
-const reducers = combineReducers({});
+/*const reducers = combineReducers({});
 const persistConfig = { key: 'DressCode', storage: AsyncStorage };
 
 const store = configureStore({
   reducer: persistReducer(persistConfig, reducers),
   middleware: (getDefaultMiddleware) => getDefaultMiddleware({ serializableCheck: false }),
+});*/ //persist Store React native à activer à la fin
+// const persistor = persistStore(store); //persist Store React native à activer à la fin
+
+const store = configureStore({
+  reducer: {},
 });
 
-const persistor = persistStore(store);
 
 
-//const Tab = createBottomTabNavigator();
 const Stack = createNativeStackNavigator();
-
+//const Tab = createBottomTabNavigator();
 /*const TabNavigator = () => {
   return (
     <Tab.Navigator screenOptions={{ headerShown: false }}>
@@ -52,26 +54,26 @@ const Stack = createNativeStackNavigator();
 export default function App() {
   return (
     <Provider store={store}>
-      <PersistGate persistor={persistor}>
+      {/*<PersistGate persistor={persistor}> //persist Store React native à activer à la fin */}
 
-        <NavigationContainer>
-          <Stack.Navigator screenOptions={{ headerShown: false }}>
-            <Stack.Screen name="SignIn" component={SignInScreen} />
-            <Stack.Screen name="Home" component={HomeScreen} />
-            <Stack.Screen name="SignUp" component={SignUpScreen} />
-            <Stack.Screen name="SearchScreen" component={SearchScreen} />
-            <Stack.Screen name="DressingScreen" component={DressingScreen} />
-            <Stack.Screen name="AddArticleScreen" component={AddArticleScreen} />
-            <Stack.Screen name="ArticleScreen" component={ArticleScreen} />
-            <Stack.Screen name="CameraScreen" component={CameraScreen} />
-            <Stack.Screen name="ImportScreen" component={ImportScreen} />
-            <Stack.Screen name="UserScreen" component={UserScreen} />
-            <Stack.Screen name="ValidateCameraScreen" component={ValidateCameraScreen} />
-            <Stack.Screen name="ValidateImportScreen" component={ValidateImportScreen} />
-          </Stack.Navigator>
-        </NavigationContainer>
+      <NavigationContainer>
+        <Stack.Navigator screenOptions={{ headerShown: false }}>
+          <Stack.Screen name="SignIn" component={SignInScreen} />
+          <Stack.Screen name="Home" component={HomeScreen} />
+          <Stack.Screen name="SignUp" component={SignUpScreen} />
+          <Stack.Screen name="SearchScreen" component={SearchScreen} />
+          <Stack.Screen name="DressingScreen" component={DressingScreen} />
+          <Stack.Screen name="AddArticleScreen" component={AddArticleScreen} />
+          <Stack.Screen name="ArticleScreen" component={ArticleScreen} />
+          <Stack.Screen name="CameraScreen" component={CameraScreen} />
+          <Stack.Screen name="ImportScreen" component={ImportScreen} />
+          <Stack.Screen name="UserScreen" component={UserScreen} />
+          <Stack.Screen name="ValidateCameraScreen" component={ValidateCameraScreen} />
+          <Stack.Screen name="ValidateImportScreen" component={ValidateImportScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
 
-      </PersistGate>
+      {/*</PersistGate> //persist Store React native à activer à la fin */}
     </Provider>
   );
 }
