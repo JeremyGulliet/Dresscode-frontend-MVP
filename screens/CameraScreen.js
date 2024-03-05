0e344fa62d261f8d9de87bcf367bb1208121e31c
Emilien Hainaux <emilienhainaux@yahoo.fr>
2024-03-05 16:22:46 +0100

    05-03-2024 16:22

--- screens/CameraScreen.js
+++ screens/CameraScreen.js
@@ -1,136 +1,154 @@
-import React, { useState, useEffect, useRef } from 'react';
-import { Button, StyleSheet, Text, View, TouchableOpacity } from 'react-native';
-import { Camera, CameraType, FlashMode } from 'expo-camera';
-import { useDispatch } from 'react-redux';
+import React, { useState, useEffect, useRef } from "react";
+import { Button, StyleSheet, Text, View, TouchableOpacity } from "react-native";
+import { Camera, CameraType, FlashMode } from "expo-camera";
+import { useDispatch } from "react-redux";
 import { useIsFocused } from "@react-navigation/native";
-import FontAwesome from 'react-native-vector-icons/FontAwesome';
+import FontAwesome from "react-native-vector-icons/FontAwesome";
 
 export default function CameraScreen({ navigation }) {
-    const dispatch = useDispatch();
-    const isFocused = useIsFocused();
+  const dispatch = useDispatch();
+  const isFocused = useIsFocused();
 
-    const [hasPermission, setHasPermission] = useState(false);
-    const [type, setType] = useState(CameraType.back);
-    const [flashMode, setFlashMode] = useState(FlashMode.off);
-    const [urlPhoto, setUrlPhoto] = useState('');
+  const [hasPermission, setHasPermission] = useState(false);
+  const [type, setType] = useState(CameraType.back);
+  const [flashMode, setFlashMode] = useState(FlashMode.off);
+  const [urlPhoto, setUrlPhoto] = useState("");
 
-    let cameraRef = useRef(null);
+  let cameraRef = useRef(null);
 
-    // Autorisation pour utiliser l'appareil photo
-    useEffect(() => {
-        (async () => {
-            const { status } = await Camera.requestCameraPermissionsAsync();
-            setHasPermission(status === 'granted');
-        })();
-    }, []);
+  // Autorisation pour utiliser l'appareil photo
+  useEffect(() => {
+    (async () => {
+      const { status } = await Camera.requestCameraPermissionsAsync();
+      setHasPermission(status === "granted");
+    })();
+  }, []);
 
-    // Fonction pour la prise de photo et l'envoi sur Cloudinairy
-    const takePicture = async () => {
-        const photo = await cameraRef.takePictureAsync({ quality: 0.3 });
-        console.log(photo);
-        const uri = photo.uri;
+  // Fonction pour la prise de photo et l'envoi sur Cloudinairy
+  const takePicture = async () => {
+    const photo = await cameraRef.takePictureAsync({ quality: 0.3 });
+    console.log(photo);
+    const uri = photo.uri;
 
+    const formData = new FormData();
 
-        const formData = new FormData();
+    formData.append("photoFromFront", {
+      uri: uri,
+      name: "photo.jpg",
+      type: "image/jpeg",
+    });
 
-        formData.append('photoFromFront', {
-            uri: uri,
-            name: 'photo.jpg',
-            type: 'image/jpeg',
-        });
+    fetch("http://192.168.1.138:3000/articles/upload", {
+      method: "POST",
+      body: formData,
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        setUrlPhoto(data);
+      });
+  };
+  // Désactiver la caméra en arrière plan quand changement d'écran
+  if (!hasPermission || !isFocused) {
+    return <View />;
+  }
 
-        fetch('http://192.168.1.41:3000/upload', {
-            method: 'POST',
-            body: formData,
-        }).then((response) => response.json())
-            .then((data) => {
-                console.log(data);
-                setUrlPhoto(data)
-            });
+  return (
+    <Camera
+      type={type}
+      flashMode={flashMode}
+      ref={(ref) => (cameraRef = ref)}
+      style={styles.camera}
+    >
+      {/* Boutons Flash et changement de camera */}
+      <View style={styles.buttonsContainer}>
+        <TouchableOpacity
+          onPress={() =>
+            setType(
+              type === CameraType.back ? CameraType.front : CameraType.back
+            )
+          }
+          style={styles.button}
+        >
+          <FontAwesome name="rotate-right" size={25} color="#ffffff" />
+        </TouchableOpacity>
 
-    }
-    // Désactiver la caméra en arrière plan quand changement d'écran
-    if (!hasPermission || !isFocused) {
-        return <View />;
-    }
+        <TouchableOpacity
+          onPress={() =>
+            setFlashMode(
+              flashMode === FlashMode.off ? FlashMode.torch : FlashMode.off
+            )
+          }
+          style={styles.button}
+        >
+          <FontAwesome
+            name="flash"
+            size={25}
+            color={flashMode === FlashMode.off ? "#ffffff" : "#e8be4b"}
+          />
+        </TouchableOpacity>
+      </View>
+      {/* Boutons retour arrière et prise de photo*/}
+      <View style={styles.bottomContainer}>
+        <View style={styles.backContainer}>
+          <TouchableOpacity
+            onPress={() => navigation.navigate("AddArticleScreen")}
+          >
+            <FontAwesome name="arrow-circle-left" size={50} color="#ffffff" />
+          </TouchableOpacity>
+        </View>
 
-
-    return (
-
-        <Camera type={type} flashMode={flashMode} ref={(ref) => cameraRef = ref} style={styles.camera}>
-            {/* Boutons Flash et changement de camera */}
-            <View style={styles.buttonsContainer}>
-                <TouchableOpacity
-                    onPress={() => setType(type === CameraType.back ? CameraType.front : CameraType.back)}
-                    style={styles.button}
-                >
-                    <FontAwesome name='rotate-right' size={25} color='#ffffff' />
-                </TouchableOpacity>
-
-                <TouchableOpacity
-                    onPress={() => setFlashMode(flashMode === FlashMode.off ? FlashMode.torch : FlashMode.off)}
-                    style={styles.button}
-                >
-                    <FontAwesome name='flash' size={25} color={flashMode === FlashMode.off ? '#ffffff' : '#e8be4b'} />
-                </TouchableOpacity>
-            </View>
-            {/* Boutons retour arrière et prise de photo*/}
-            <View style={styles.bottomContainer}>
-                <View style={styles.backContainer}>
-                    <TouchableOpacity onPress={() => navigation.navigate('AddArticleScreen')}>
-                        <FontAwesome name='arrow-circle-left' size={50} color='#ffffff' />
-                    </TouchableOpacity>
-                </View>
-
-                <View style={styles.snapContainer}>
-                    <TouchableOpacity onPress={() => cameraRef && takePicture() && navigation.navigate('ValidateCameraScreen', { urlPhoto })}>
-                        <FontAwesome name='circle-thin' size={95} color='#ffffff' />
-                    </TouchableOpacity>
-                </View>
-            </View>
-        </Camera>
-
-    )
+        <View style={styles.snapContainer}>
+          <TouchableOpacity
+            onPress={() =>
+              cameraRef &&
+              takePicture() &&
+              navigation.navigate("ValidateCameraScreen", { urlPhoto })
+            }
+          >
+            <FontAwesome name="circle-thin" size={95} color="#ffffff" />
+          </TouchableOpacity>
+        </View>
+      </View>
+    </Camera>
+  );
 }
 
 const styles = StyleSheet.create({
-    camera: {
-        flex: 1,
-    },
-    buttonsContainer: {
-        flex: 0.1,
-        flexDirection: 'row',
-        alignItems: 'flex-end',
-        justifyContent: 'space-between',
-        paddingTop: 50,
-        paddingLeft: 20,
-        paddingRight: 20,
-    },
-    button: {
-        width: 44,
-        height: 44,
-        alignItems: 'center',
-        justifyContent: 'center',
-        backgroundColor: 'rgba(0, 0, 0, 0.2)',
-        borderRadius: 50,
-    },
-    snapContainer: {
-        marginRight: 50,
+  camera: {
+    flex: 1,
+  },
+  buttonsContainer: {
+    flex: 0.1,
+    flexDirection: "row",
+    alignItems: "flex-end",
+    justifyContent: "space-between",
+    paddingTop: 50,
+    paddingLeft: 20,
+    paddingRight: 20,
+  },
+  button: {
+    width: 44,
+    height: 44,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "rgba(0, 0, 0, 0.2)",
+    borderRadius: 50,
+  },
+  snapContainer: {
+    marginRight: 50,
+  },
 
-    },
+  backContainer: {
+    marginRight: 100,
+  },
 
-    backContainer: {
-        marginRight: 100,
-
-    },
-
-    bottomContainer: {
-        flex: 1,
-        flexDirection: 'row',
-        alignItems: 'flex-end',
-        justifyContent: 'space-evenly',
-        marginRight: 100,
-        marginBottom: 50,
-
-    }
+  bottomContainer: {
+    flex: 1,
+    flexDirection: "row",
+    alignItems: "flex-end",
+    justifyContent: "space-evenly",
+    marginRight: 100,
+    marginBottom: 50,
+  },
 });
--- screens/ValidateCameraScreen.js
+++ screens/ValidateCameraScreen.js
@@ -1,90 +1,90 @@
-import { Button, StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View, TouchableOpacity } from "react-native";
 import HeaderCompo from "../components/headerCompo.js";
 import FooterCompo from "../components/footerCompo.js";
-import FontAwesome from 'react-native-vector-icons/FontAwesome';
+import FontAwesome from "react-native-vector-icons/FontAwesome";
 
 export default function ValidateCameraScreen({ navigation }) {
-    return (
-        <View style={styles.mainContainer}>
-            <View style={styles.notifBar}>
-                <View style={styles.headerContainer}>
-                    <HeaderCompo />
-                </View>
-            </View>
-
-            <View style={styles.contentContainer}>
-                <View style={styles.imageContainer}>
-                    <Text>IMAGE</Text>
-                </View>
-                <View style={styles.descriptionContainer}>
-                    <Button title="Go To Camera"
-                        onPress={() => navigation.navigate('CameraScreen')}
-                    />
-
-                    <Button title="Go To Dressing"
-                        onPress={() => navigation.navigate('DressingScreen')}
-                    />
-                </View>
-
-            </View>
-
-
-            <View style={styles.backContainer}>
-                <TouchableOpacity onPress={() => navigation.navigate('AddArticleScreen')}>
-                    <FontAwesome name='arrow-circle-left' size={50} color='#ffffff' />
-                </TouchableOpacity>
-            </View>
-
+  return (
+    <View style={styles.mainContainer}>
+      <View style={styles.notifBar}>
+        <View style={styles.headerContainer}>
+          <HeaderCompo />
         </View>
-    )
+      </View>
+
+      <View style={styles.contentContainer}>
+        <View style={styles.imageContainer}>
+          <Text>IMAGE</Text>
+        </View>
+        <View style={styles.descriptionContainer}>
+          <Button
+            title="Go To Camera"
+            onPress={() => navigation.navigate("CameraScreen")}
+          />
+
+          <Button
+            title="Go To Dressing"
+            onPress={() => navigation.navigate("DressingScreen")}
+          />
+        </View>
+      </View>
+
+      <View style={styles.backContainer}>
+        <TouchableOpacity
+          onPress={() => navigation.navigate("AddArticleScreen")}
+        >
+          <FontAwesome name="arrow-circle-left" size={50} color="#ffffff" />
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    mainContainer: {
-        flex: 1,
-        backgroundColor: "#fff",
-        flexDirection: "column",
-        alignItems: "center",
-        justifyContent: "center",
-    },
-    notifBar: {
-        flex: 2,
-        display: "flex",
-        flexDirection: "column",
-        justifyContent: "flex-end",
-        backgroundColor: "#0E0E66",
-        // marginTop: 40,
-    },
+  mainContainer: {
+    flex: 1,
+    backgroundColor: "#fff",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  notifBar: {
+    flex: 2,
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "flex-end",
+    backgroundColor: "#0E0E66",
+    // marginTop: 40,
+  },
 
-    headerContainer: {
-        flex: 1,
-        marginTop: 40,
-    },
+  headerContainer: {
+    flex: 1,
+    marginTop: 40,
+  },
 
-    contentContainer: {
-        flex: 12,
-        flexDirection: "column",
-        justifyContent: "center",
-        alignItems: "center",
-        backgroundColor: "whitesmoke",
-        width: "100%",
-    },
-    footerContainer: {
-        flex: 1,
-        backgroundColor: "green",
-        width: "100%",
-    },
+  contentContainer: {
+    flex: 12,
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "whitesmoke",
+    width: "100%",
+  },
+  footerContainer: {
+    flex: 1,
+    backgroundColor: "green",
+    width: "100%",
+  },
 
-    imageContainer: {
-        flex: 2,
-        backgroundColor: 'blue',
-        width: "100%",
-    },
+  imageContainer: {
+    flex: 2,
+    backgroundColor: "blue",
+    width: "100%",
+  },
 
-    descriptionContainer: {
-        flex: 5,
-        backgroundColor: 'red',
-        width: "100%"
-    }
-
+  descriptionContainer: {
+    flex: 5,
+    backgroundColor: "red",
+    width: "100%",
+  },
 });
