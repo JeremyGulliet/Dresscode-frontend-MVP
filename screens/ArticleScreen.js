2530a543cfe60cea01f17c09bdaa695df93b95ec
Eburi <123583827+L6X86L@users.noreply.github.com>
2024-03-13 17:18:15 +0100

    first commit

--- screens/ArticleScreen.js
+++ screens/ArticleScreen.js
@@ -1,33 +1,96 @@
-import { Button, StyleSheet, Text, View, Image } from "react-native";
+import {
+  Button,
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  SafeAreaView,
+  ScrollView,
+} from "react-native";
+import HeaderCompo from "../components/headerCompo";
+import { Ionicons } from "@expo/vector-icons";
 
 export default function ArticleScreen({ navigation, route }) {
   const { item } = route.params ? route.params : { item: null }; // Vérification de route.params
   console.log({ item });
 
+  // Fonction pour revenir vers dressing
+  const handleBackPress = () => {
+    navigation.navigate("DressingScreen");
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Article SCREEN</Text>
-      {item && ( // Afficher l'image si "item" est "true"
-        <Image source={{ uri: item.url_image }} style={styles.imageDressing} />
-      )}
-      <Button
-        title="Go To Dressing"
-        onPress={() => navigation.navigate("DressingScreen")}
-      />
-    </View>
+    <SafeAreaView style={{ flex: 1 }}>
+      <ScrollView contentContainerStyle={{ flexGrow: 1 }}>
+        <View style={styles.container}>
+          {/* header */}
+          <View style={styles.headerContainer}>
+            <HeaderCompo navigation={navigation} />
+          </View>
+
+          <View style={styles.imgContainer}>
+            <View style={styles.iconContainer}>
+              <Ionicons
+                onPress={handleBackPress}
+                name="arrow-back"
+                size={30}
+                color="black"
+              />
+            </View>
+            {/* Afficher l'image */}
+            {item && item.url_image ? (
+              <Image
+                source={{ uri: item.url_image }}
+                style={styles.imageDressing}
+              />
+            ) : (
+              <Image
+                source={require("../assets/dressing/top-04.png")}
+                style={styles.imageDressing}
+              />
+            )}
+          </View>
+
+          <Button
+            title="Go To Dressing"
+            onPress={() => navigation.navigate("DressingScreen")}
+          />
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
+  headerContainer: {
+    height: 100,
+  },
   container: {
     flex: 1,
     backgroundColor: "#fff",
     alignItems: "center",
+    // justifyContent: "center",
+  },
+  imgContainer: {
+    backgroundColor: "grey",
+    paddingVertical: 20,
+    width: "100%",
+    borderBottomWidth: 1.5,
+    marginBottom: 30,
+    flexDirection: "row",
+    alignItems: "center",
     justifyContent: "center",
   },
+  iconContainer: {
+    position: "absolute",
+    top: 0,
+    left: 0,
+    padding: 10,
+  },
+
   imageDressing: {
-    height: 150,
-    width: 150,
+    height: 200,
+    width: 200,
     resizeMode: "contain",
     borderRadius: 8,
     marginHorizontal: 10,
